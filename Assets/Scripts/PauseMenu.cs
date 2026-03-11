85afbaa26b382c958d23a8e56393303314c40829
Tal Gordon <nuvenin@proton.me>
2026-03-11 19:04:00 +0200

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    [Header("Input")]
+    public InputAction pauseAction;
+
+    private void OnEnable()
+    {
+        pauseAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pauseAction.Disable();
+    }
+
+    void Update()
+    {
+        if (pauseAction.WasPressedThisFrame())
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        // Important: Ensure time scale is reset before loading a new scene, 
+        // otherwise the new scene might start paused!
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
