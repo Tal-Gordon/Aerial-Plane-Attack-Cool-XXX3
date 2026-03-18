76fde6415e5131114a42399e3e1772bdaedbee8e
Tal Gordon <nuvenin@proton.me>
2026-03-18 23:06:24 +0200

    please kill me

---
+++ Assets/Scripts/UI/GameModeSelectionController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class GameModeSelectionController : MonoBehaviour
+{
+    [Header("Hero Panel Elements")]
+    public TextMeshProUGUI heroTitleText;
+    public TextMeshProUGUI heroDescriptionText;
+    public Image heroImage;
+
+    [Header("List Setup")]
+    public Transform buttonContainer; // The layout group holding your buttons
+    public GameObject buttonPrefab;   // A prefab of a single UI button
+    public List<GameModeData> availableModes; // Drag your ScriptableObjects here
+
+    void Start()
+    {
+        PopulateList();
+
+        // Auto-select the first mode so the screen isn't empty when it loads
+        if (availableModes.Count > 0)
+        {
+            SelectMode(availableModes[0]);
+        }
+    }
+
+    void PopulateList()
+    {
+        // Loop through all our modes and spawn a button for each
+        foreach (GameModeData mode in availableModes)
+        {
+            GameObject newBtnObj = Instantiate(buttonPrefab, buttonContainer);
+            GameModeButton btnScript = newBtnObj.GetComponent<GameModeButton>();
+
+            // Pass the data and a reference to 'this' controller
+            btnScript.Setup(mode, this);
+        }
+    }
+
+    // This gets called by the individual buttons when clicked
+    public void SelectMode(GameModeData newlySelectedMode)
+    {
+        // Update the Single Hero Panel with the new data
+        heroTitleText.text = newlySelectedMode.modeName;
+        heroDescriptionText.text = newlySelectedMode.description;
+        heroImage.sprite = newlySelectedMode.heroArtwork;
+
+        // Note: This is exactly where you would add your DOTween or LeanTween
+        // code to fade the text in or briefly scale up the hero image!
+    }
+}
--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -4,12 +4,21 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject buttons;
+    public GameObject selectionWindow;
 
-    private string gameScene = "Game";
-
-    public void LoadGame()
+    public void ToggleSelectionMenu()
     {
-        SceneManager.LoadScene(gameScene);
+        selectionWindow.SetActive(!selectionWindow.activeSelf);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OpenSettings()
+    {
+        // TODO
     }
 
     public void QuitGame()
