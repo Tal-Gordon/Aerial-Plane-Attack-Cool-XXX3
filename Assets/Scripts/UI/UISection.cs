a5050c8fca4fa90213acdd878a44a76abf9e5306
Tal Gordon <nuvenin@proton.me>
2026-03-28 12:47:09 +0300

    just saving my progress, ignore

--- Assets/Scripts/UI/UISection.cs
+++ Assets/Scripts/UI/UISection.cs
@@ -8,18 +8,41 @@ public class UISection : FoldablePanel
 
     [Header("Config")]
     [SerializeField] private string sectionTitle = "Section Title"; // default value
+    [SerializeField] private GameObject widgetSeperatorPrefab;
+
+    private UIWidget[] childWidgets;
 
     protected override void Awake()
     {
         base.Awake();
         if (headerLabel) headerLabel.text = sectionTitle;
+        childWidgets = GetComponentsInChildren<UIWidget>(includeInactive: true);
+
+        if (widgetSeperatorPrefab != null && childWidgets.Length > 1)
+        {
+            for (int i = 0; i < childWidgets.Length - 1; i++)
+            {
+                UIWidget currentWidget = childWidgets[i];
+                if (currentWidget != null && currentWidget.transform.parent != null)
+                {
+                    GameObject seperator = Instantiate(widgetSeperatorPrefab, currentWidget.transform.parent);
+                    seperator.name = "WidgetSeperator";
+                    seperator.transform.SetSiblingIndex(currentWidget.transform.GetSiblingIndex() + 1);
+                }
+            }
+        }
     }
 
     // Propagate Tick() downward to only this section's widgets
     public void TickWidgets(SimulationSnapshot snapshot)
     {
         if (IsFolded) return;
-        foreach (var widget in GetComponentsInChildren<UIWidget>(includeInactive: false))
-            widget.Tick(snapshot);
+        foreach (var widget in childWidgets)
+        {
+            if (widget.gameObject.activeInHierarchy)
+            {
+                widget.Tick(snapshot);
+            }
+        }
     }
 }
