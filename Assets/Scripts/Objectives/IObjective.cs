5e2bc3c840dddbb8bfde4e1277424953975aecbf
dehilke <yoavi.cohen100@gmail.com>
2026-04-03 23:04:13 +0300

    completed the objective

--- Assets/Scripts/Objectives/IObjective.cs
+++ Assets/Scripts/Objectives/IObjective.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public interface IObjective
 {
-    public float CalculateTotalFitness(JetAgent agent);
+    public void SetStartingState(JetAgent agent, int index, int totalPopulation, Vector3 centerPoint);
 
     public float GetStepReward(JetAgent agent);
 
-    public bool CheckTerminalState(JetAgent agent);
+    public float CalculateTotalFitness(JetAgent agent);
 
-    public void SetStartingState(JetAgent agent, int index, int totalPopulation, Vector3 centerPoint);
+    public bool CheckTerminalState(JetAgent agent);
 }
