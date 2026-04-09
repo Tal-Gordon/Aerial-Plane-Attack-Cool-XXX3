17d7bcc4a02aad478c5d2b579d2da98f15632b8a
dehilke <yoavi.cohen100@gmail.com>
2026-04-09 13:55:33 +0300

    bunch of bullshit

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -42,8 +42,8 @@ public static class DataManager
         {
             [GameMode.MaxAltitude] = new SimulationSettings
             {
-                PopulationSize = 20,
-                MutationRate = 0.5f,
+                PopulationSize = 2000,
+                MutationRate = 0.1f,
                 NetworkShape = new[] { 12, 24, 12, 4 },   // inputs, hidden, outputs
                 AIType = AIType.FixedNeuroEvo,
                 SpawnRadius = 50f,
@@ -51,7 +51,7 @@ public static class DataManager
             },
             [GameMode.FlightSchool] = new SimulationSettings
             {
-                PopulationSize = 1000,
+                PopulationSize = 2000,
                 MutationRate = 0.1f,
                 NetworkShape = new[] { 19, 16, 16, 4 },
                 AIType = AIType.FixedNeuroEvo,
